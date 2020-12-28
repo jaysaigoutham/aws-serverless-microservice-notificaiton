8a790a140a228a3b36cded128dd9dda1a7cee42a
jayasai <jayasai.goutheman@cammsgroup.com>
2020-12-29 03:27:43 +0530

    Fixing SES function code

--- msc-assignment/SendEmail.js
+++ msc-assignment/SendEmail.js
@@ -1,6 +1,8 @@
 const AWS = require('aws-sdk');
 const SES = new AWS.SES({ /*region: 'us-east-2'*/ });
 
+const sample_verified_email = 'jayasai.kliq@gmail.com'; //will be used for demo purpose since SNS & SES only allows verified emails in sandbox
+
 exports.handler = async (event, context) => {
     
     console.log(JSON.stringify(event, null, 2));
@@ -10,18 +12,32 @@ exports.handler = async (event, context) => {
         console.log('DynamoDB Record: %j', record.dynamodb);
     });
 
-  const to = 'jayasai.kliq@gmail.com';
-  const from = 'taest@test.com';
-  const reply_to = 'jayasai.kliq@gmail.com';
-  const subject = 'test';
+  if(event.Records.lengh == 0)
+  {
+    console.error("ERROR : No records found");
+    return;
+  }
+
+  var record = event.Records[0];
+  console.log("record : "+ JSON.stringify(record));
+  console.log("record Email : "+ JSON.stringify(record.dynamodb.NewImage.NotificationEmail.S));
+  console.log("record Body: "+ JSON.stringify(record.dynamodb.NewImage.NotificationBody.S));
+
+  const to = record.dynamodb.NewImage.NotificationEmail.S; //FYI : only verififed emails work in sandbox env
+  const from = record.dynamodb.NewImage.NotificationEmail.S; //FYI : only verififed emails work in sandbox env
+  const reply_to = record.dynamodb.NewImage.NotificationEmail.S; //FYI : only verififed emails work in sandbox env
+  const subject = record.dynamodb.NewImage.NotificationBody.S;
  
-  const fromBase64 = Buffer.from(from).toString('base64');
+  //const fromBase64 = Buffer.from(from).toString('base64');
 
   const htmlBody = `
     <!DOCTYPE html>
     <html>
-      <head></head>
-      <body><h1>Hello world!</h1></body>
+      <head>Greetings from Fake Uber !</head>
+      <body>
+        <h1>New notificaiton from your Fake Uber platfrom!</h1>
+        <h2>` + subject +`</h2>
+      </body>
     </html>
   `;
 
@@ -42,7 +58,7 @@ exports.handler = async (event, context) => {
       },
     },
     //ReplyToAddresses: [replyTo],
-    Source: 'jayasai.kliq@gmail.com',
+    Source: to,
   };
   const response = await SES.sendEmail(sesParams).promise();
   console.log(response);
